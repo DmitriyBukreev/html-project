cc7506788b98f3008581a37a2128a867a3eaef6f
DmitriyBukreev <dmitribukree@yandex.ru>
2023-04-21 18:58:56 +0300

    Code refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,35 @@
-const PugPlugin = require("pug-plugin");
-const FileManagerPlugin = require("filemanager-webpack-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const PugPlugin = require('pug-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   entry: {
-    index: path.join(__dirname, "src", "index.pug"),
+    index: path.join(__dirname, 'src', 'index.pug'),
   },
 
   output: {
-    path: path.join(__dirname, "dist"),
+    path: path.join(__dirname, 'dist'),
     assetModuleFilename: path.join(
-      "assets",
-      "images",
-      "[name].[contenthash][ext]"
+      'assets',
+      'images',
+      '[name].[contenthash][ext]',
     ),
+  },
+
+  resolve: {
+    alias: {
+      '@utils': path.resolve(__dirname, 'src', 'utils'),
+      '@images': path.resolve(__dirname, 'src', 'assets', 'images'),
+    },
   },
 
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: "babel-loader",
+        use: 'babel-loader',
         exclude: /node_modules/,
       },
       {
@@ -35,24 +42,24 @@ module.exports = {
         // sass-loader    - traspiles sass to valid css
         // postcss-loader - makes use of it's preset-env plugin to make css compatible with other browsers
         // css-loader     - interprets all @import and url() statements
-        use: ["css-loader", "postcss-loader", "sass-loader"],
+        use: ['css-loader', 'postcss-loader', 'sass-loader'],
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.svg$/,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: path.join("icons", "[name].[contenthash][ext]"),
+          filename: path.join('icons', '[name].[contenthash][ext]'),
         },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: path.join("fonts", "[name].[contenthash][ext]"),
+          filename: path.join('fonts', '[name].[contenthash][ext]'),
         },
       },
     ],
@@ -62,13 +69,13 @@ module.exports = {
     new FileManagerPlugin({
       events: {
         onStart: {
-          delete: ["dist"],
+          delete: ['dist'],
         },
         onEnd: {
           copy: [
             {
-              source: path.join("src", "assets", "static"),
-              destination: "dist",
+              source: path.join('src', 'assets', 'static'),
+              destination: 'dist',
             },
           ],
         },
@@ -78,16 +85,16 @@ module.exports = {
       // mini-css-extract-plugin replacement
       css: {
         // output filename of CSS files
-        filename: "assets/css/[name].[contenthash:8].css",
+        filename: 'assets/css/[name].[contenthash:8].css',
       },
       js: {
-        filename: "assets/js/[name].[contenthash:8].css",
+        filename: 'assets/js/[name].[contenthash:8].css',
       },
     }),
   ],
 
   devServer: {
-    watchFiles: path.join(__dirname, "src"),
+    watchFiles: path.join(__dirname, 'src'),
     port: 9000,
   },
 
@@ -98,10 +105,10 @@ module.exports = {
           implementation: ImageMinimizerPlugin.imageminMinify,
           options: {
             plugins: [
-              ["gifsicle", { interlaced: true }],
-              ["jpegtran", { progressive: true }],
-              ["optipng", { optimizationLevel: 5 }],
-              ["svgo", { name: "preset-default" }],
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
+              ['svgo', { name: 'preset-default' }],
             ],
           },
         },
